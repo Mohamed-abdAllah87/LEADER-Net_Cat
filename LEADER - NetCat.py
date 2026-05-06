be2df45b87994fb41666b0ed1a738249e5242d2f
Mohamed AbdAllah <moh.ahmed87@gmail.com>
2026-05-06 07:45:50 +0300

    Create LEADER - NetCat.py
    The famous NC tool

---
+++ LEADER - NetCat.py
@@ -0,0 +1,142 @@
+import socket
+import subprocess
+import argparse
+import sys
+import threading
+import shlex
+import textwrap
+
+def execute (cmd):
+    cmd = cmd.strip()
+    if not cmd:
+        return
+    output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
+    return output.decode()
+
+class NetCat:
+    def __init__(self, args, buffer=None):
+        self.args =args
+        self.buffer=buffer
+        self.socket= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    def run(self):
+        if self.args.listen:
+            self.listen()
+        else:
+            self.send()
+
+    def send (self):
+        self.socket.connect((self.args.target, self.args.port))
+
+        if self.buffer:
+            self.socket.send(self.buffer)
+
+            try:
+                while True:
+                    recv_len = (4096)
+                    response = ''
+                    while recv_len:
+                        data = self.socket.recv(4096)
+                        recv_len = len(data)
+                        response += data.decode()
+                        if recv_len < 4096:
+                              break
+                        
+                    if response:
+                        print(response)
+                        buffer = input ('> ')
+                        buffer += '\n'
+                        self.socket.send(buffer.encode())
+
+            except KeyboardInterrupt:
+                print ('\nUser Terminated')
+                self.socket.close()
+                sys.exit()
+
+    def listen (self):
+        self.socket.bind((self.args.target, self.args.port))
+        self.socket.listen(5)
+        print(f'[*] Now Listening on {self.args.target}:{self.args.port}')
+
+        try:
+            while True:
+                client_socket, _ = self.socket.accept()
+                handle_thread = threading.Thread(target=self.handle, args=(client_socket,))
+                handle_thread.start()
+
+        except KeyboardInterrupt:
+                self.socket.close()
+                sys.exit()
+            
+    def handle(self, client_socket):
+        if self.args.execute:
+            output = execute(self.args.execute)
+            client_socket.send(output.encode())
+
+        elif self.args.upload:
+            file_buffer = b''
+            while True:
+                data = client_socket.recv(4096)
+                if data:
+                    file_buffer += data
+                else:
+                    break
+            
+            with open(self.args.upload, 'wb') as f:
+                f.write(file_buffer)
+            client_socket.send(f'Saved file {self.args.upload}'.encode())
+
+        elif self.args.command:
+            cmd_buffer = b''
+            while True:
+                try:
+                    client_socket.send(b'#LEED_SHELL: #> ')
+                    
+                    while '\n' not in cmd_buffer.decode():
+                        data = client_socket.recv(64)
+                        if not data: 
+                            break
+                        cmd_buffer += data
+
+                    clean_cmd = cmd_buffer.decode().strip()
+                    
+                    if clean_cmd:
+                        response = execute(clean_cmd)
+                        if response:
+                            client_socket.send(response.encode())
+                    
+                    cmd_buffer = b''
+                    
+                except Exception as e:
+                    print(f'[*] Connection closed for this client: {e}')
+                    client_socket.close() 
+                    break 
+
+if __name__ == '__main__' :
+
+    parser = argparse.ArgumentParser(
+        description='BHP Net Tool',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=textwrap.dedent('''Exaple:
+            netcat.py -t 192.168.1.108 -p 5555 -l -c # command shell
+            netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt #upload file
+            netcat.py -t 192.168.1.108 -p 5555 -l -e=\"cat /etcpasswd\" #execute comman
+            echo 'ABC' | ./netcat.py -t 192.168.1.108 -p 135 3 echo text to server port
+            netcat.py -t 192.168.1.108 -p 5555 # connect to server '''))
+    
+    parser.add_argument('-c', '--command', action='store_true', help='command shell')
+    parser.add_argument('-e', '--execute', help='execute specified command')
+    parser.add_argument('-l', '--listen', action='store_true', help='listen')
+    parser.add_argument('-p', '--port', type=int, default=5555, help='specified port')
+    parser.add_argument('-t', '--target', default='192.168.1.203', help='specified IP')
+    parser.add_argument('-u', '--upload', help='upload file')
+
+    args = parser.parse_args()
+    if args.listen:
+        buffer = ''
+    else:
+        buffer = sys.stdin.read() if not sys.stdin.isatty() else ''
+
+    nc = NetCat(args, buffer.encode())
+    nc.run()
